ea6b69f3289a51b311db4bc67f203f8c3da18e10
Hans Gaiser <hansg91@gmail.com>
2021-12-19 23:54:06 +0100

    Add auto command for reloading packages.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,28 +6,28 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.api.nvim_command('packadd packer.nvim')
 end
 
--- -- Autocommand that reloads neovim whenever you save the plugins/init.lua file.
--- vim.cmd([[
--- 	augroup packer_user_config
--- 		autocmd!
--- 		autocmd BufWritePost */plugins/init.lua source <afile> | PackerSync
--- 	augroup end
--- ]])
+-- Autocommand that reloads neovim whenever you save the plugins/init.lua file.
+vim.cmd([[
+	augroup packer_user_config
+		autocmd!
+		autocmd BufWritePost */plugins/init.lua source <afile> | PackerSync
+	augroup end
+]])
 
--- -- Use a protected call so we don't error out on first use.
--- local status_ok, packer = pcall(require, "packer")
--- if not status_ok then
--- 	return
--- end
+-- Use a protected call so we don't error out on first use.
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
 
--- -- Have packer use a popup window
--- packer.init {
--- 	display = {
--- 		open_fn = function()
--- 			return require("packer.util").float { border = "rounded" }
--- 		end,
--- 	},
--- }
+-- Have packer use a popup window
+packer.init {
+	display = {
+		open_fn = function()
+			return require("packer.util").float { border = "rounded" }
+		end,
+	},
+}
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself

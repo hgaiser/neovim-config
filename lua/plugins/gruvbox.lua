7d7732182d3aa6f7d11a78f5f9acb14e6cf08ff4
Hans Gaiser <hansg91@gmail.com>
2022-01-17 17:30:20 +0100

    Give in, use gruvbox white.

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -6,10 +6,6 @@ vim.opt.termguicolors = true
 vim.cmd([[
 colorscheme gruvbox
 
-" Set white text color to be a bit brighter.
-hi Normal guifg=#ede7d8
-hi GruvboxFg1 guifg=#ede7d8
-
 " Show operators (+, -, =, etc.) in blue for easier reading.
 hi link Operator GruvboxBlue
 

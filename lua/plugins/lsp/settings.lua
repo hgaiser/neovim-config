24cea503f1c6b1713d190487c026277ad5e813d5
Hans Gaiser <hansg91@gmail.com>
2022-06-01 10:48:14 +0200

    Add keybinding for open float diagnostic.

--- lua/plugins/lsp/settings.lua
+++ lua/plugins/lsp/settings.lua
@@ -3,6 +3,7 @@ utils.map { 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>' }
 utils.map { 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>' }
 utils.map { 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>' }
 utils.map { 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>' }
+utils.map { 'n', 'g?', '<cmd>lua vim.diagnostic.open_float()<CR>' }
 
 -- Show line diagnostics on hover.
 -- vim.cmd([[

24fe4092a3eb67812d27f11bd8e02d812efeab61
Hans Gaiser <hansg91@gmail.com>
2022-06-01 11:40:27 +0200

    Make autocompletion for tab/s-tab the same as up/down.

--- lua/plugins/lsp/cmp.lua
+++ lua/plugins/lsp/cmp.lua
@@ -1,4 +1,6 @@
 local cmp = require('cmp')
+local types = require('cmp.types')
+
 cmp.setup({
 	-- Enable LSP snippets
 	snippet = {
@@ -7,10 +9,10 @@ cmp.setup({
 		end,
 	},
 	mapping = {
-		['<Up>'] = cmp.mapping.select_prev_item(),
-		['<Down>'] = cmp.mapping.select_next_item(),
-		['<S-Tab>'] = cmp.mapping.select_prev_item(),
-		['<Tab>'] = cmp.mapping.select_next_item(),
+		['<Down>'] = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Select }),
+		['<Up>'] = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Select }),
+		['<Tab>'] = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Select }),
+		['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Select }),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<CR>'] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Insert,

039bee0b5e64f122000859e2762b5241b71f5f3d
Hans Gaiser <hansg91@gmail.com>
2022-07-05 13:27:18 +0200

    Add dap configuration for C / C++.

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -1,3 +1,25 @@
+local dap = require('dap')
+dap.adapters.lldb = {
+	type = 'executable',
+	command = '/usr/bin/lldb-vscode', -- adjust as needed, must be absolute path
+	name = 'lldb'
+}
+
+dap.configurations.cpp = {
+	{
+		name = 'Launch',
+		type = 'lldb',
+		request = 'launch',
+		program = function()
+			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		end,
+		cwd = '${workspaceFolder}',
+		stopOnEntry = false,
+		args = {},
+	},
+}
+dap.configurations.c = dap.configurations.cpp
+
 vim.fn.sign_define('DapBreakpoint', {text='⏹', texthl='GruvboxRed', linehl='', numhl=''})
 vim.fn.sign_define('DapBreakpointCondition', {text='⏹', texthl='GruvboxOrange', linehl='', numhl=''})
 vim.fn.sign_define('DapLogPoint', {text='⏹', texthl='GruvboxBlue', linehl='', numhl=''})

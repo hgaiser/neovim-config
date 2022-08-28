482eb3e26ea4065a0e6a04f96e932039ea93be90
Hans Gaiser <hansg91@gmail.com>
2022-08-28 21:34:25 +0200

    Add textobject for block.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,6 +16,8 @@ require('nvim-treesitter.configs').setup {
 				-- You can use the capture groups defined in textobjects.scm
 				['af'] = '@function.outer',
 				['if'] = '@function.inner',
+				['ab'] = '@block.outer',
+				['ib'] = '@block.inner',
 				['ac'] = '@class.outer',
 				['ic'] = '@class.inner',
 				['i,'] = '@parameter.inner',

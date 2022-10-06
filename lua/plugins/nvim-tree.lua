54f5a1e7f3df81524db29e4ac4ee5ae31a9d451d
Hans Gaiser <hansg91@gmail.com>
2022-10-06 10:21:47 +0200

    Update nvim-tree.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,42 +1,56 @@
-vim.g.nvim_tree_show_icons = {
-	git = 1,
-	folders = 1,
-	files = 0,
-	folder_arrows = 0,
-}
-vim.g.nvim_tree_icons = {
-	git = {
-		unstaged = '●',
-		staged = '●',
-		unmerged = '●',
-		renamed = '➜',
-		untracked = '◌',
-		deleted = '●',
-		ignored = '◌'
-	},
-	folder = {
-		arrow_open = '▾',
-		arrow_closed = '▸',
-		default = '▸',
-		open = '▾',
-		empty = '▸',
-		empty_open = '▾',
-		symlink = '▸',
-		symlink_open = '▾',
-	}
-}
-vim.g.nvim_tree_indent_markers = 1
-
 require('nvim-tree').setup {
-	auto_close = true,
 	filters = {
 		dotfiles = true,
 	},
 	git = {
 		ignore = false,
 	},
+	renderer = {
+		indent_markers = {
+			enable = true,
+		},
+		icons = {
+			show = {
+				git = true,
+				folder = true,
+				file = false,
+				folder_arrow = false,
+			},
+			glyphs = {
+				git = {
+					unstaged = '●',
+					staged = '●',
+					unmerged = '●',
+					renamed = '➜',
+					untracked = '◌',
+					deleted = '●',
+					ignored = '◌'
+				},
+				folder = {
+					arrow_open = '▾',
+					arrow_closed = '▸',
+					default = '▸',
+					open = '▾',
+					empty = '▸',
+					empty_open = '▾',
+					symlink = '▸',
+					symlink_open = '▾',
+				},
+			},
+		},
+	},
 }
 
 utils.map {'n', '<Leader>n', ':NvimTreeToggle<CR>'  , silent = true}
 utils.map {'n', '`'        , ':NvimTreeToggle<CR>'  , silent = true}
 utils.map {'n', '<Leader>`', ':NvimTreeFindFile<CR>', silent = true}
+
+-- Close nvim if nvim-tree is the last buffer.
+vim.api.nvim_create_autocmd("BufEnter", {
+	nested = true,
+	callback = function()
+		if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+			vim.cmd "quit"
+		end
+	end
+})

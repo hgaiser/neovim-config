48c93547c62e6aba6ee2f12fa6c91bf09e47edf7
Hans Gaiser <hansg91@gmail.com>
2022-10-06 10:21:36 +0200

    Update gitsigns.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -115,7 +115,7 @@ return packer.startup(function(use)
 		'lewis6991/gitsigns.nvim',
 		config = [[require('plugins.gitsigns')]],
 		requires = { 'nvim-lua/plenary.nvim' },
-		tag = 'v0.4',
+		tag = 'v0.5',
 	}
 
 	-- File tree viewer.

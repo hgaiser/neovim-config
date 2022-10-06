1f9179ee7b1cdb394d0d80e81b971706b629ebe4
Hans Gaiser <hansg91@gmail.com>
2022-10-06 10:08:43 +0200

    Remove deprecated option.

--- lua/plugins/lsp/languages/rust.lua
+++ lua/plugins/lsp/languages/rust.lua
@@ -2,7 +2,6 @@
 require('rust-tools').setup({
 	tools = { -- rust-tools options
 		autoSetHints = true,
-		hover_with_actions = true,
 		inlay_hints = {
 			show_parameter_hints = true,
 			parameter_hints_prefix = '',

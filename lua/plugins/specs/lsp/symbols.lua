ad70b87e21432be8863acbab73713c78f9ca6fe0
Hans Gaiser <hansg91@gmail.com>
2023-02-20 22:56:10 +0100

    Remove code symbols plugin for now.

--- lua/plugins/specs/lsp/symbols.lua
+++
@@ -1,8 +0,0 @@
-return {
-	-- Show LSP symbols in a tree view.
-	{
-		"simrat39/symbols-outline.nvim",
-		cmd = "SymbolsOutline",
-		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols" } },
-	},
-}

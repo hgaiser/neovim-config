e9fb9699cb0743036ca6f1de7aa93d68535bf313
Hans Gaiser <hansg91@gmail.com>
2023-02-21 20:32:51 +0100

    Tweak icons for neo-tree.

--- lua/plugins/specs/neo-tree.lua
+++ lua/plugins/specs/neo-tree.lua
@@ -3,19 +3,19 @@ return {
 		"nvim-neo-tree/neo-tree.nvim",
 		cmd = "Neotree",
 		keys = {
-			{
-				"`",
-				function()
-					require("neo-tree.command").execute({ toggle = true, reveal = true })
-				end,
-				desc = "Filebrowser",
-			},
+			{ "`", function() require("neo-tree.command").execute({ toggle = true, reveal = true }) end, desc = "Filebrowser", },
 		},
 		opts = {
 			enable_git_status = true,
 			enable_diagnostics = false,
 			close_if_last_window = true,
 			default_component_configs = {
+				icon = {
+					folder_closed = "▶",
+					folder_open = "▼",
+					folder_empty = "▼",
+					default = " ",
+				},
 				container = {
 					enable_character_fade = false
 				},

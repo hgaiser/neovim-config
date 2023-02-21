22f2ff4d100a0e0ff9187dcbce634247d39ea705
Hans Gaiser <hansg91@gmail.com>
2023-02-21 20:36:01 +0100

    Add configuration for trouble.nvim.

--- lua/plugins/specs/lsp/trouble.lua
+++ lua/plugins/specs/lsp/trouble.lua
@@ -1,3 +1,21 @@
 return {
-	{ "folke/trouble.nvim", },
+	{
+		"folke/trouble.nvim",
+		opts = {
+			icons = false,
+			fold_open = "▼",
+			fold_closed = "▶",
+			indent_lines = false,
+			signs = {
+				error = "error",
+				warning = "warn",
+				hint = "hint",
+				information = "info"
+			},
+			use_diagnostic_signs = false
+		},
+		keys = {
+			{ "<leader>ct", "<cmd>TroubleToggle<cr>", desc = "Trouble"}
+		},
+	},
 }

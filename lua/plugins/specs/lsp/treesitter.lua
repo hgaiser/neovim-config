e8451fed234825227b376a3ddf9c07f0eaaf82b7
Hans Gaiser <hansg91@gmail.com>
2023-02-24 09:55:32 +0100

    Add textobjects from treesitter.

--- lua/plugins/specs/lsp/treesitter.lua
+++ lua/plugins/specs/lsp/treesitter.lua
@@ -33,10 +33,33 @@ return {
 					node_decremental = "<bs>",
 				},
 			},
+
+			textobjects = {
+				select = {
+					enable = true,
+
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						['af'] = '@function.outer',
+						['if'] = '@function.inner',
+						['ab'] = '@block.outer',
+						['ib'] = '@block.inner',
+						['ac'] = '@class.outer',
+						['ic'] = '@class.inner',
+						['i,'] = '@parameter.inner',
+						['a,'] = '@parameter.outer',
+					},
+				},
+			},
 		},
 		---@param opts TSConfig
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup(opts)
 		end,
 	},
+
+	{ "nvim-treesitter/nvim-treesitter-textobjects" },
 }

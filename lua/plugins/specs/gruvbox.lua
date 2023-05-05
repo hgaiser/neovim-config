c2b6a0996fb953c02b516568eb9ac8454ffb8fa0
Hans Gaiser <hansg91@gmail.com>
2023-05-05 16:00:38 +0200

    Fix highlight of floating windows.

--- lua/plugins/specs/gruvbox.lua
+++ lua/plugins/specs/gruvbox.lua
@@ -12,6 +12,10 @@ return {
 		},
 		init = function()
 			vim.cmd.colorscheme("gruvbox")
+
+			-- Don't know why, but floating windows have no background.
+			-- This links the style to that of Pmenu, which looks okay.
+			vim.api.nvim_set_hl(0, "NormalFloat", { link = "Pmenu" })
 		end,
 	},
 }
